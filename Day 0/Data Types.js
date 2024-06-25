620e57294549cdd2e7e221cf2a5536861080e038
Abhinav Kumar <abhinavkumar2369@outlook.com>
2024-06-25 18:27:39 +0000

    Done ✅

---
+++ Day 0/Data Types.js
@@ -0,0 +1,14 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+
+    const firstInteger = 4;
+    const sum = firstInteger + Number(secondInteger)
+    console.log(sum);
+    
+    const firstDecimal = 4.0;
+    const sum2 = firstDecimal + Number(secondDecimal);
+    console.log(sum2);
+    
+    const firstString = 'HackerRank ';
+    console.log(firstString+secondString);
+       
+}
